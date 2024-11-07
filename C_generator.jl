120e35b55e98948ba54fed147cf7548ab2e32a94
ZeyaZheng <zeya.zheng@gmail.com>
2024-11-07 22:54:39 +0200

    organize codes make it more readble

--- C_generator.jl
+++ C_generator.jl
@@ -32,6 +32,7 @@ fis = @sugfis function tipper(service, food)::tip
     service == excellent || food == delicious --> tip == generous
 end
 
+# Membership function expression, need to add more
 function to_c(mf::GaussianMF)
     """
     double GaussianMF(double x, double mean, double sigma) {
@@ -52,6 +53,38 @@ function to_c(mf::TrapezoidalMF)
     """
 end
 
+#Generate membership function
+function generate_mf_definitions(fis::SugenoFuzzySystem)
+    visited = DataType[]
+    res = ""
+    for (var_name, var) in pairs(fis.inputs)
+        for (mf_name, mf) in pairs(var.mfs)
+            if !(typeof(mf) in visited)
+                res *= to_c(mf) * "\n\n"
+                push!(visited, typeof(mf))
+            end
+        end
+    end
+    return res
+end
+
+#Generate Fuzzification part
+function collect_properties(x)
+    join([getproperty(x, p) for p in propertynames(x)], ", ")
+end
+
+function generate_fuzzification(fis::SugenoFuzzySystem)
+    res = ""
+    for (var_name, var) in pairs(fis.inputs)
+        for (mf_name, mf) in pairs(var.mfs)
+            line = "\tdouble $(var_name)_$mf_name = $(nameof(typeof(mf)))($var_name, $(collect_properties(mf)));"
+            res *= line * "\n"
+        end
+    end
+    return res
+end
+
+# generate rule evaluation part 1
 function generate_rule_expression(r::FuzzyRelation)
     prop = r.prop
     subj = r.subj
@@ -76,6 +109,30 @@ function generate_rule_expression(r::FuzzyNegation)
     return "1-$(subj)_$prop"
 end
 
+#Generate rule evaluation part 2
+function generate_rules(fis::SugenoFuzzySystem)
+    rules_vector = fis.rules
+    rules_c_expression = "\n"
+    for i in eachindex(rules_vector)
+        rule = rules_vector[i]
+        ant_return = generate_rule_expression(rule.antecedent)
+        rules_c_expression *= "\tdouble rule$i = $ant_return;\n"
+    end
+    return rules_c_expression * "\n"
+end
+
+function generate_rules_consequent(fis::SugenoFuzzySystem)
+    rules_vector = fis.rules
+    rules_c_expression = "\n"
+    for i in eachindex(rules_vector)
+        rule = rules_vector[i]
+        cons_return = generate_rule_expression(rule.consequent[1])
+        rules_c_expression *= "\tdouble r$(i)_out = $cons_return;\n"
+    end
+    return rules_c_expression
+end
+
+#Generate rule outputs
 function generate_rule_expression(r::ConstantSugenoOutput)
     return "$(r.c)"
 end
@@ -88,7 +145,6 @@ function generate_rule_expression(r::LinearSugenoOutput)
     return rule_out
 end
 
-# generate sugeno outputs C code part
 function generate_outputs(fis::SugenoFuzzySystem)
     rule_out = ""
     for (var_name, var) in pairs(fis.outputs)
@@ -102,6 +158,7 @@ function generate_outputs(fis::SugenoFuzzySystem)
     return rule_out * "\n"
 end
 
+#Generate Weighted average calculation
 function generate_calculation(fis::SugenoFuzzySystem)
     calculation = ""
     for (var_name, var) in pairs(fis.outputs)
@@ -120,65 +177,7 @@ function generate_calculation(fis::SugenoFuzzySystem)
     return calculation * "\n"
 end
 
-function generate_rules(fis::SugenoFuzzySystem)
-    rules_vector = fis.rules
-    rules_c_expression = "\n"
-
-    for i in eachindex(rules_vector)
-        rule = rules_vector[i]
-        ant_return = generate_rule_expression(rule.antecedent)
-
-        rules_c_expression *= "\tdouble rule$i = $ant_return;\n"
-    end
-    return rules_c_expression * "\n"
-end
-
-function collect_properties(x)
-    join([getproperty(x, p) for p in propertynames(x)], ", ")
-end
-
-function generate_fuzzification(fis::SugenoFuzzySystem)
-    res = ""
-    for (var_name, var) in pairs(fis.inputs)
-        for (mf_name, mf) in pairs(var.mfs)
-            # @show var_name, mf_name, mf
-            # println(mf)
-            # println(fieldnames(typeof(mf)))
-            line = "\tdouble $(var_name)_$mf_name = $(nameof(typeof(mf)))($var_name, $(collect_properties(mf)));"
-            # line = "double food_rancid = GaussianMF(food, 1.0, 2.0);" # TODO: fix me
-            res *= line * "\n"
-        end
-    end
-    return res
-end
-
-function generate_mf_definitions(fis::SugenoFuzzySystem)
-    visited = DataType[]
-    res = ""
-    for (var_name, var) in pairs(fis.inputs)
-        for (mf_name, mf) in pairs(var.mfs)
-            if !(typeof(mf) in visited)
-                res *= to_c(mf) * "\n\n"
-                push!(visited, typeof(mf))
-            end
-        end
-    end
-    return res
-end
-
-function generate_rules_consequent(fis::SugenoFuzzySystem)
-    rules_vector = fis.rules
-    rules_c_expression = "\n"
-
-    for i in eachindex(rules_vector)
-        rule = rules_vector[i]
-        cons_return = generate_rule_expression(rule.consequent[1])
-        rules_c_expression *= "\tdouble r$(i)_out = $cons_return;\n"
-    end
-    return rules_c_expression
-end
-
-# main generator function
+# main C_generator function
 function generate_tip(fis::SugenoFuzzySystem)
     res = ""
     func_def = generate_mf_definitions(fis)
